03eb57a186b166c5c02734dcd260d03aca9afba4
syedharishafeez <syed_haris98@yahoo.com>
2020-08-16 19:41:46 +0500

    complete UI with Next Button Functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,138 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { Grid, Typography, Paper, Button } from "@material-ui/core";
 
 function App() {
+  let [score, setScore] = useState(0);
+  let [totalQuestions, setTotalQuestions] = useState<
+    {
+      incorrect_answers: String[];
+      correct_answer: String;
+      question: String;
+    }[]
+  >([]);
+  let [questionNumber, setQuestionNumber] = useState(1);
+  let [question, setQuestion] = useState<String>();
+  let [choices, setChoices] = useState<String[]>([]);
+  useEffect(() => {
+    (async () => {
+      let questions = await fetch(
+        "https://opentdb.com/api.php?amount=10&difficulty=easy&type=multiple"
+      );
+      let jsonQuestions = await questions.json();
+      let results: {
+        incorrect_answers: String[];
+        correct_answer: String;
+        question: String;
+      }[] = jsonQuestions.results;
+      let incorrect_answers: String[] =
+        results[questionNumber - 1].incorrect_answers;
+      let correct_answer: String = results[questionNumber - 1].correct_answer;
+      let questionAsked: String = results[questionNumber - 1].question;
+      setTotalQuestions(results);
+      setChoices([...incorrect_answers, correct_answer]);
+      setQuestion(questionAsked);
+      // setChoices([
+      //   ...jsonQuestions.results.incorrect_answers,
+      //   jsonQuestions.results.correct_answer,
+      // ]);
+      console.log("questions = ", jsonQuestions.results);
+      return;
+    })();
+  }, []);
+
+  function onNextButton() {
+    let incorrect_answers: String[] =
+      totalQuestions[questionNumber].incorrect_answers;
+    let correct_answer: String = totalQuestions[questionNumber].correct_answer;
+    let questionAsked: String = totalQuestions[questionNumber].question;
+    setTotalQuestions(totalQuestions);
+    setChoices([...incorrect_answers, correct_answer]);
+    setQuestion(questionAsked);
+    setQuestionNumber(questionNumber + 1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid container>
+      <Grid container spacing={3} justify="center" alignItems="center">
+        <Grid item xs={12}>
+          <Typography variant="h1" style={{ textAlign: "center" }}>
+            React Quiz
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant="h4" style={{ textAlign: "center" }}>
+            Score : {score}
+          </Typography>
+        </Grid>
+        <Grid container justify="center" alignItems="center">
+          <Grid item xs={6}>
+            <Paper
+              variant="outlined"
+              style={{
+                width: "100%",
+                backgroundColor: "#ecf7fc",
+                padding: "14px 0px 20px 0px",
+              }}
+            >
+              <Grid container spacing={3} justify="center" alignItems="center">
+                <Grid item xs={12}>
+                  <Typography variant="h5" style={{ textAlign: "center" }}>
+                    Questions {questionNumber}/{totalQuestions.length}
+                  </Typography>
+                </Grid>
+                <Grid item xs={12}>
+                  <Typography variant="h5" style={{ textAlign: "center" }}>
+                    {question}
+                  </Typography>
+                </Grid>
+              </Grid>
+              <Grid container spacing={2} justify="center" alignItems="center">
+                {choices.map((item) => (
+                  <Grid item xs={11}>
+                    <Paper
+                      variant="outlined"
+                      style={{
+                        width: "100%",
+                        backgroundColor: "#00BFFF",
+                        textAlign: "center",
+                        padding: "5px 0px 5px 0px",
+                      }}
+                    >
+                      {item}
+                    </Paper>
+                  </Grid>
+                ))}
+              </Grid>
+            </Paper>
+          </Grid>
+        </Grid>
+        <Grid item>
+          {questionNumber < totalQuestions.length ? (
+            <Button
+              variant="contained"
+              color="primary"
+              style={{ textAlign: "center" }}
+              onClick={() => {
+                onNextButton();
+              }}
+            >
+              Next Question
+            </Button>
+          ) : (
+            <Button
+              variant="contained"
+              color="primary"
+              style={{ textAlign: "center" }}
+              // onClick={() => {
+              //   onNextButton();
+              // }}
+            >
+              Finish
+            </Button>
+          )}
+        </Grid>
+      </Grid>
+    </Grid>
   );
 }
 
